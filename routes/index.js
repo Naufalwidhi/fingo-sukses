5bacf4f7739b582b23b23a493a3e4dc0bbd03699
Aziz Fikri <afikrim10@student.ub.ac.id>
2020-07-15 01:50:58 +0700

    Delete everything

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-const createError = require('http-errors')
 const express = require('express')
 
 const router = express.Router()
@@ -15,32 +14,10 @@ router.get('/', (req, res, next) => {
   }
 })
 
-router.get('/sistem', (req, res, next) => {
-  try {
-    res.render('pages/sistem', {
-      title: 'Sistem - Fingo Sukses',
-      layout: 'layouts/main',
-    })
-  } catch (error) {
-    next(error)
-  }
-})
-
 router.get('/marketing-plan', (req, res, next) => {
   try {
     res.render('pages/marketing', {
       title: 'Marketing Plan - Fingo Sukses',
-      layout: 'layouts/main',
-    })
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.get('/video-fingo', (req, res, next) => {
-  try {
-    res.render('pages/video', {
-      title: 'Video Fingo - Fingo Sukses',
       layout: 'layouts/main',
     })
   } catch (error) {
