ab728435c90d13b3b6922dcb875a0cdd5517aff8
Aziz Fikri <afikrim10@student.ub.ac.id>
2020-07-15 01:49:54 +0700

    I dont even know

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,37 @@ router.get('/', (req, res, next) => {
   }
 })
 
+router.get('/sistem', (req, res, next) => {
+  try {
+    res.render('pages/sistem', {
+      title: 'Sistem - Fingo Sukses',
+      layout: 'layouts/main',
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/marketing-plan', (req, res, next) => {
+  try {
+    res.render('pages/marketing', {
+      title: 'Marketing Plan - Fingo Sukses',
+      layout: 'layouts/main',
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/video-fingo', (req, res, next) => {
+  try {
+    res.render('pages/video', {
+      title: 'Video Fingo - Fingo Sukses',
+      layout: 'layouts/main',
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = router
